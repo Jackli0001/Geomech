3332f0b982de6d920a00cb75711df872e351a030
tianqizx <tianqizx@gmail.com>
2017-09-13 17:26:46 -0500

    Update prob1.m

--- prob1.m
+++ prob1.m
@@ -9,7 +9,7 @@ c = 90;
 
 R_pg = [cosd(a)*cosd(b) sind(a)*cosd(b) -sind(b);
   cosd(a)*sind(b)*sind(c)-sind(a)*cosd(c) sind(a)*sind(b)*sind(c)+cosd(a)*cosd(c) cosd(b)*sind(c);
-  cosd(a)*sind(b)*cosd(c)+sind(a)*sind(c) sind(a)*sind(b)*sind(c)-cosd(a)sind(c) cosd(b)cosd(c)];
+  cosd(a)*sind(b)*cosd(c)+sind(a)*sind(c) sind(a)*sind(b)*sind(c)-cosd(a)*sind(c) cosd(b)*cosd(c)];
 
 Sp = diag([78.9,66.6,64.3]);
 Sg = R_pg'*Sp*R_pg;
