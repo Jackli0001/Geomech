83e937bf4769cb9e17fd83c23e5a4019a74b98b1
tianqizx <tianqizx@gmail.com>
2017-09-14 13:53:28 -0500

    0914

--- prob1.m
+++ prob1.m
@@ -4,17 +4,26 @@ clc
 %% prob 1
 % converting the stress tensor to geographical coordinate system
 
+Pp = 55.7;
+SHmax = 78.9;
+Sv = 66.6;
+Shmin = 64.3;
+
 a = 90;
 b = 0;
 c = 90;
+
 
 R_pg = [cosd(a)*cosd(b) sind(a)*cosd(b) -sind(b);
   cosd(a)*sind(b)*sind(c)-sind(a)*cosd(c) sind(a)*sind(b)*sind(c)+cosd(a)*cosd(c) cosd(b)*sind(c);
   cosd(a)*sind(b)*cosd(c)+sind(a)*sind(c) sind(a)*sind(b)*sind(c)-cosd(a)*sind(c) cosd(b)*cosd(c)];
 
-Sp = diag([78.9,66.6,64.3]);
+Sp = diag([SHmax,Sv,Shmin]);
 Sg = R_pg'*Sp*R_pg;
 
 %% using a uniform distribution
 strike = rand(100,1) * 360;
 dip = rand(100,1) * 90;
+
+nn = [-sind(strike)*sind(dip);cosd(strike)*sind(dip);-cosd(dip)];
+t = Sg * nn;
